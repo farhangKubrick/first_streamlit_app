af4092565178726ecea645e97fb570338a4b1856
Farhan <farhangohar@kubrickgroup.com>
2023-06-09 17:49:46 +0100

    adding a fruits list csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
-
+import pandas as pd
+my_fruits_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruits_list)
 streamlit.title('My parents new healthy diner')
 streamlit.header('Header 1')
 streamlit.text('text 1')
 streamlit.text('text 2')
 streamlit.text('from Vs code')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
