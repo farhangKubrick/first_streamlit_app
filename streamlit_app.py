3ba36ca902ef8fef540e3179f5545d0ede61f1d8
Farhan <farhangohar@kubrickgroup.com>
2023-06-09 18:05:26 +0100

    Adding pre-selected fruits to list

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,9 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include
-#The list is based on the index of myfruitlist, which we have set to the 'fruit' column of the csv 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#The list is based on the index of myfruitlist, which we have set to the 'fruit' column of the csv
+# If we add a list in our third parameter it will autopopulate with those 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 streamlit.dataframe(my_fruit_list)
 streamlit.title('My parents new healthy diner')
 streamlit.header('Header 1')
