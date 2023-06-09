113f118a50cb02b209c69f0f5f84f6b44da49c22
Farhan <farhangohar@kubrickgroup.com>
2023-06-09 18:02:39 +0100

    adding the index as the fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit
 import pandas as pd
-my_fruits_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruits_list.index))
-streamlit.dataframe(my_fruits_list)
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#Setting the index of our list to the fruit name in the table
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here so they can pick the fruit they want to include
+#The list is based on the index of myfruitlist, which we have set to the 'fruit' column of the csv 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
 streamlit.title('My parents new healthy diner')
 streamlit.header('Header 1')
 streamlit.text('text 1')
