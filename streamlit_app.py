ba06f05bbdc7c6bb678f14e6ea5a71381150cc63
Farhan <farhangohar@kubrickgroup.com>
2023-06-18 13:58:14 +0100

    added a variable to API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,10 +22,13 @@ streamlit.text('from Vs code')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #this onl writes data to the screen
 #streamlit.text(fruityvice_response.json())
 
 #This converts the json to a tabular format
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
