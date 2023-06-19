5ffac20dc485e3cd917390e4002f42c5136ed465
Farhan <farhangohar@kubrickgroup.com>
2023-06-19 12:54:03 +0100

    test adding stuff to list

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,6 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 #Letting the user pick a fruit
 add_my_fruit = streamlit.text_input('What fruit would you like information about?')
-my_data_row.append(add_my_fruit)
+my_data_row.append(str(add_my_fruit))
 streamlit.text("Thanks for adding " + add_my_fruit)
 streamlit.dataframe(my_data_row)
