a429b9825ecd8625a0fcca9a85704b788e88ccd5
Farhan <farhangohar@kubrickgroup.com>
2023-06-19 13:24:45 +0100

    Added a try except block to fruityvice resp

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,20 @@ streamlit.text('from Vs code')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#this onl writes data to the screen
-#streamlit.text(fruityvice_response.json())
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        #this onl writes data to the screen
+        #streamlit.text(fruityvice_response.json())
+        #This converts the json to a tabular format
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
-#This converts the json to a tabular format
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
 #Not running any code apst this
 streamlit.stop()
 
