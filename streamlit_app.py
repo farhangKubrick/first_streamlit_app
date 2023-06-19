7a6cd9a50090a97678a921a0b83d6b68e91a65f2
Farhan <farhangohar@kubrickgroup.com>
2023-06-19 13:48:57 +0100

    testing a fix for adding new fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,10 +64,18 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-#Letting the user pick a fruit
-# add_my_fruit = streamlit.text_input('What fruit would you like information about?')
-# streamlit.text("Thanks for adding " + add_my_fruit)
-# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#A function to add a fruit to the snowflake table
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('"new_fruit"')")
+        return('Thanks for adding ' + new_fruit)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+    #Creating a connection to snowflake
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = insert_row_snowflake(add_my_fruit)
+    streamlit.text(my_data_rows)
+
 
 #Not running any code apst this
 streamlit.stop()
