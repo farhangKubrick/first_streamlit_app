ab4b9c534ac5b86805cd6b37b76d1d64112ce8dc
Farhan <farhangohar@kubrickgroup.com>
2023-06-19 12:59:14 +0100

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 #Letting the user pick a fruit
 add_my_fruit = streamlit.text_input('What fruit would you like information about?')
-
+streamlit.text(type(add_my_fruit))
 my_data_row.append(tuple(str(add_my_fruit)))
 streamlit.text("Thanks for adding " + add_my_fruit)
 streamlit.text(my_data_row)
